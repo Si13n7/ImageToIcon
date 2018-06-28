c2e84f0941051e6a722290c58e41485e794c6922
Si13n7 <admin@si13n7.com>
2018-06-29 00:59:58 +0200

    Update assembly version numbers.

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("18.3.4.0")]
-[assembly: AssemblyFileVersion("18.3.4.0")]
+[assembly: AssemblyVersion("18.6.29.0")]
+[assembly: AssemblyFileVersion("18.6.29.0")]
